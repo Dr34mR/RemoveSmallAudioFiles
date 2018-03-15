3f1de705cddb5e993617ae4e2d75a5b8f56445f4
Scott Loomes <scott.loomes@inreachsw.com>
2018-03-15 20:21:29 +1000

    Initial Commit

---
+++ RemoveSmallAudioFiles/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace RemoveSmallAudioFiles
+{
+    internal class Program
+    {
+        private const long SizeInKBytes = 500;
+        private static string _startingDirectory = string.Empty;
+        private static long _filesRemoved = 0;
+
+        private static void Main(string[] args)
+        {
+            _startingDirectory = Directory.GetCurrentDirectory();
+            Console.Title = $"Scanning {_startingDirectory}";
+
+            foreach (var dir in Directory.GetDirectories(_startingDirectory)) { DoLoop(dir); }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\n{_filesRemoved} file/s removed");
+        }
+
+        private static void DoLoop(string directoryToScan)
+        {
+            if (!Directory.Exists(directoryToScan)) return;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"*{directoryToScan.Substring(_startingDirectory.Length)}\\");
+
+            CheckFiles(directoryToScan);
+            foreach (var subDir in Directory.GetDirectories(directoryToScan)) { DoLoop(subDir); }
+        }
+
+        private static void CheckFiles(string directoryToCheck)
+        {
+            if (!Directory.Exists(directoryToCheck)) return;
+
+            foreach (var file in Directory.GetFiles(directoryToCheck))
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.Length > 1024 * SizeInKBytes) continue;
+                File.Delete(file);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"*{file.Substring(_startingDirectory.Length)}");
+                _filesRemoved += 1;
+            }
+        }
+    }
+}
